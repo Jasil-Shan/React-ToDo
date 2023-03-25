84932014181e79fba6dbb99077d52a2a0cfe3d81
Jasil Shan <jasilshan786@gmail.com>
2023-03-25 10:33:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useState } from "react";
 function App() {
+  const [input, setInput] = useState("");
+  const [Todos, setTodos] = useState([]);
+
+  const addTodo = () => {
+    setTodos([...Todos, { text: input, status: false }]);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+        <h2>If there's a will ,There's a way </h2>
+        <br />
+      </div>
+      <div className="input">
+        <input
+          type="text"
+          value={input}
+          placeholder="🖊️ Add item..."
+          onChange={(event) => setInput(event.target.value)}
+        />
+        <i onClick={addTodo} className="fas fa-plus"></i>
+      </div>
+      <div className="todos">
+        {Todos.map((value) => {
+          return (
+            <div className="todo">
+              <div className="left">
+                <input type="checkbox" name="" id="" />
+                <p>{value}</p>
+              </div>
+              <div className="right">
+                <i className="fas fa-times"></i>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
-
 export default App;
