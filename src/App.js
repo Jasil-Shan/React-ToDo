c31ffa89c57f3614ca33e85fc2bfc8178816c6f1
Jasil Shan <jasilshan786@gmail.com>
2023-03-26 12:40:18 +0530

    Added Delete Feature

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import { useState } from "react";
 function App() {
   const [input, setInput] = useState("");
   const [Todos, setTodos] = useState([]);
-
   const addTodo = () => {
-    setTodos([...Todos, { text: input, status: false }]);
+    if (input.trim() !== "") {
+      setTodos([...Todos, { id: Date.now(), text: input, status: false }]);
+      setInput("");
+    }
   };
+
   return (
     <div className="app">
       <div className="mainHeading">
@@ -27,18 +30,61 @@ function App() {
         <i onClick={addTodo} className="fas fa-plus"></i>
       </div>
       <div className="todos">
-        {Todos.map((value) => {
+        {Todos.map((obj) => {
           return (
             <div className="todo">
               <div className="left">
-                <input type="checkbox" name="" id="" />
-                <p>{value}</p>
+                <input
+                  value={obj.status}
+                  onChange={(e) => {
+                    setTodos(
+                      Todos.filter((obj2) => {
+                        if (obj2.id === obj.id) {
+                          obj2.status = e.target.checked;
+                          console.log(obj2.status);
+                        }
+                        return obj2;
+                      })
+                    );
+                  }}
+                  type="checkbox"
+                  name=""
+                  id=""
+                />
+                <p>{obj.text}</p>
               </div>
               <div className="right">
-                <i className="fas fa-times"></i>
+                <i
+                  onClick={() =>
+                    setTodos(
+                      Todos.filter((val) => {
+                        if (val.id === obj.id) {
+                          val = "";
+                        }
+                        return val;
+                      })
+                    )
+                  }
+                  className="fas fa-times"
+                ></i>
               </div>
             </div>
           );
+        })}
+        <br />
+        <h2>Completed Tasks</h2>
+        <br />
+        {Todos.map((obj) => {
+          if (obj.status) {
+            return (
+              <div>
+                <ul className="cmp">
+                  <li>{obj.text}</li>
+                </ul>
+              </div>
+            );
+          }
+          return null;
         })}
       </div>
     </div>
